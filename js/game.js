f6053fd041921e9e33844a1b2fbb245468bf21e3
EverettBernard <124380095+EverettBernard@users.noreply.github.com>
2023-05-31 14:43:45 +0000

    added comments to our files along with a favicon

--- js/game.js
+++ js/game.js
@@ -1,10 +1,30 @@
 /* global Phaser*/
 
+// Copyright (c) 2023 Everett Bernard All right reserved
+//
+// Created by: Everett Bernard
+// Created on: June 2023
+// Tis is the Phaser3 configuration file
+// This file contains the JS functions for index.html for chicken shooter
+
+//* Game scene */
 const config = {
   type: Phaser.AUTO,
   width: 1920,
   height: 1080,
-  backgroundColor: 0x5f6e7a
+  physics: {
+    default: "arcade",
+    arcade: {
+      debug: true
+    }
+  },
+  // set background color
+  backgroundColor: 0x513e7f,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    // we place it in the middle of the page.
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  }
 }
 
 const game = new Phaser.Game(config)
